b044867590d4a250bc5852e4303bdad90118417e
Ivan <oiv@live.ru>
2020-04-01 21:10:32 +0500

    v 0.2
    Устранены глобальные переменные.
    
    Теперь повторяющиеся ингредиенты правильно суммируются.

--- main.py
+++ main.py
@@ -1,52 +1,35 @@
 
 
-cook_book = {}
+def assemble_cook_book(file_name):
+    with open(file_name, encoding='utf8') as f:
+        cook_book = {}
+        while True:
+            dish_name = f.readline().strip()
+            if not dish_name:
+                break
+            num_ingr = int(f.readline().strip())
+            cook_book[dish_name] = []
+            while num_ingr != 0:
+                dish_ingr = f.readline().split(sep='|')
+                dish_ingr = [x.strip() for x in dish_ingr]
+                dish_ingr_dic = {'ingredient_name': dish_ingr[0], 'quantity': int(dish_ingr[1]), 'measure': dish_ingr[2]}
+                cook_book[dish_name].append(dish_ingr_dic)
+                num_ingr -= 1
+            f.readline()
+    return cook_book
 
-with open('recipes.txt', encoding='utf8') as f:
 
-    while True:
-        dish_name = f.readline().strip()
-        if not dish_name:
-            break
-        num_ingr = int(f.readline().strip())
-        cook_book[dish_name] = []
-        while num_ingr != 0:
-            dish_ingr = f.readline().split(sep='|')
-            dish_ingr = [x.strip() for x in dish_ingr]
-            dish_ingr_dic = {'ingredient_name': dish_ingr[0], 'quantity': int(dish_ingr[1]), 'measure': dish_ingr[2]}
-            cook_book[dish_name].append(dish_ingr_dic)
-            num_ingr -= 1
-        f.readline()
-
-
-def shop_book(recipe_request, recipe_list):
+def calc_shop_list(num_pers, dish_request, recipe_list):
     shop_list = {}
-    for dish in recipe_list:
-        if dish in recipe_request:
-            shop_list[dish] = recipe_list[dish]
-    return shop_list
+    for dish in dish_request:
+        if dish in recipe_list.keys():
+            for ingr in recipe_list[dish]:
+                name = ingr['ingredient_name']
+                if name in shop_list:
+                    shop_list[name]['quantity'] += ingr['quantity'] * num_pers
+                else:
+                    shop_list[ingr['ingredient_name']] = {'quantity': ingr['quantity'] * num_pers, 'measure': ingr['measure']}
+    print(shop_list)
 
+calc_shop_list(4, ['Омлет', 'Омлет', 'Омлет', 'Запеченный картофель'], assemble_cook_book('recipes.txt'))
 
-def calc_shop_list(person_quantity, recipe_list):
-    shop_list = {}
-    for dish in recipe_list.values():
-        for ingredient in dish:
-            shop_ingredient = ingredient
-            shop_ingredient['quantity'] *= person_quantity
-            if shop_ingredient['ingredient_name'] not in shop_list:
-                shop_list[shop_ingredient['ingredient_name']] = shop_ingredient
-            else:
-                shop_list[shop_ingredient['ingredient_name']]['quantity'] += shop_ingredient['quantity']
-    return shop_list
-
-
-def print_shop_list(shop_list):
-    print('Нужно купить:')
-    for ingredient in shop_list.values():
-        print(f"{ingredient['ingredient_name']}: {ingredient['quantity']} {ingredient['measure']}")
-
-test_shop = calc_shop_list(3, shop_book(['Омлет','Запеченный картофель'], cook_book))
-
-print(test_shop)
-
-print_shop_list(test_shop)
